270d20a1c998892ac68852b978b129e9b831f1ac
ehsan1988 <birnaghdejavan@gmail.com>
2018-07-22 11:08:17 +0430

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+/*
+module.exports = {
+  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  secretOrKey: 'secret'
+};
+*/
+
+module.exports = {
+  mongoURI: 'mongodb://ehsan:123456@ds227570.mlab.com:27570/socialconnect',
+  secretKey: 'ehsan',
+  localhost: 'mongodb://localhost/ehsan'
+};
